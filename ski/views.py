948a621c5457eeb46066de7ef3949483c771597f
JosiasAurel <ndjosiasaurel@gmail.com>
2020-11-15 11:02:38 +0100

    creage project starter

---
+++ ski/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Snippet
+# Create your views here.
+
+def home(request):
+    return render(request, "index.html", {})
+
+def skis(request):
+    ski_objs = Snippet.objects.all()
+    _ski_ctx = {
+        'skis': ski_objs
+    }
+    return render(request, "skis.html", _ski_ctx)
