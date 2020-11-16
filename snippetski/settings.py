bd96a081a40c1a4491518a1ef7a4e32d255e9ad6
Josias Aurel <ndjosiasaurel@gmail.com>
2020-11-16 18:09:33 +0100

    make dev logs to false for hosting

--- snippetski/settings.py
+++ snippetski/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'r%z4two2=*-h26sf2zt==c@4=^i(w%3_1cio=n*mz*q*r)y_ur'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
