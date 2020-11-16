2dacce7466a08f99ddeb379822884d38d31b2a61
JosiasAurel <ndjosiasaurel@gmail.com>
2020-11-16 03:24:55 +0100

    delete migration, __pycache__ and re-migrate db

--- ski/models.py
+++ ski/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Snippet(models.Model):
-      title = models.CharField(max_length=200)
-      body = models.CharField(max_length=1000)
-      created_on = models.DateTimeField(auto_now=True)
-      last_modified = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=200)
+    body = models.CharField(max_length=1000)
+    created_on = models.DateTimeField(auto_now=True)
+    last_modified = models.DateTimeField(auto_now_add=True)
 
-      def __str__(self):
-          return self.title
+    def __str__(self):
+        return self.title
 
-
-      
+    """ def __repr__(self):
+        return f"<Snippet {self.title} at {self.created_on}>" """
